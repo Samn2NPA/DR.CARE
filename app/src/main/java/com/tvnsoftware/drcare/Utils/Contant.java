56199a30f5f1a6fb8b0e2402fcc8d07d1599bc6a
Thieusike-Pro\Thieusike <Tam101901@gmail.com>
2017-07-11 00:59:09 +0700

    [TamHH] initial project

---
+++ app/src/main/java/com/tvnsoftware/drcare/Utils/Contant.java
@@ -0,0 +1,10 @@
+package com.tvnsoftware.drcare.Utils;
+
+/**
+ * Created by Thieusike on 7/10/2017.
+ */
+
+public class Contant {
+    public static final String BASE_URL = "https://api.nytimes.com/svc/search/v2/";
+    public static final String API_KEY = "c42ba6d82eff4a30bb66b26ba0c8201f";
+}
---
+++ app/src/main/java/com/tvnsoftware/drcare/api/RestApiEndpointInterface.java
@@ -0,0 +1,10 @@
+package com.tvnsoftware.drcare.api;
+
+/**
+ * Created by Thieusike on 7/10/2017.
+ */
+
+public interface RestApiEndpointInterface {
+//    @GET("articlesearch.json")
+//    Call<Article> getArtical(@QueryMap Map<String, String> options);
+}
